e29cc651aded89ef56ee41737b2724faf667d905
Ville Salonen <ville.salonen@iki.fi>
2015-03-19 20:10:49 +0200

    Added optional scale factor argument

--- convert.py
+++ convert.py
@@ -3,16 +3,24 @@
 import sys
 from PIL import Image
 
-if (len(sys.argv) != 3):
-    print "Usage: ./convert.py source.asc dest.png"
+if (len(sys.argv) not in [3, 4]):
+    print "Usage: ./convert.py source.asc dest.png [height scaling]"
     exit()
 
-def scale(input):
+def scale(input, scale_factor):
     input += 40
-    return input / 1024.0 * 2**16 * 4
+    return input / 1024.0 * 2**16 * scale_factor
 
 source_path = sys.argv[1]
 dest_path = sys.argv[2]
+
+scale_factor = 1.0
+if len(sys.argv) == 4:
+    scale_factor_str = sys.argv[3]
+    try:
+        scale_factor = float(scale_factor_str)
+    except:
+        print "Could not parse double value from %s. Defaulting to %d" % (scale_factor_str, scale_factor)
 
 file = open(source_path)
 content = [x.strip('\n') for x in file.readlines()]
@@ -20,7 +28,7 @@ heights = content[6:]
 
 map = Image.new('I', (3000,3000))
 
-heights_ints = [[scale(int(float(x))) for x in row.strip().split(" ")] for row in heights]
+heights_ints = [[scale(int(float(x)), scale_factor) for x in row.strip().split(" ")] for row in heights]
 
 offset = 0
 for row in heights_ints:
